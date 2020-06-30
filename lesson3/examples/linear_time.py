6364bc9b09bdfa317a7cda4bb7fc8a640b767511
Matheus Reis <matheus.reis@olist.com>
2020-06-30 14:30:48 -0300

    Added another example for linear time

--- lesson3/examples/linear_time.py
+++ lesson3/examples/linear_time.py
@@ -5,4 +5,17 @@ def linear(n, A):
     
     return 1
 
+
+def linear2(n, m):
+    result = 0
+
+    for i in range(n):
+        result += i
+    for j in range(m):
+        result += j
+    
+    return result
+
+
 print(linear(10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(linear2(10, 20))
