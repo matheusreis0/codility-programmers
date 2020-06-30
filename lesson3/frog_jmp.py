d38b4e7ec62dfc45112b3eea23c95c50733079e2
Matheus Reis <matheus.reis@olist.com>
2020-06-30 17:17:39 -0300

    Frog jump with 100%

--- lesson3/frog_jmp.py
+++ lesson3/frog_jmp.py
@@ -1,15 +1,18 @@
 def solution(X, Y, D):
     jumps = 0
 
-    while X < Y:
-        jumps += 1
-        X += D
+    diff = Y - X
+
+    if diff % D == 0:
+        jumps = diff // D
+    else:
+        jumps = diff // D + 1
 
     return jumps
 
 
-x = 1
-y = 5
-d = 2
+x = 10
+y = 85
+d = 30
 
 print(solution(x, y, d))
