69d6c98d2713232da865010b168fc9368853b22b
Matheus Reis <matheus.reis@olist.com>
2020-08-03 17:52:26 -0300

    tape equilibrium with 100 perfomance

--- lesson3/tape_equilibrium.py
+++ lesson3/tape_equilibrium.py
@@ -1,10 +1,34 @@
-def solution(A):
-    values = []
-    for x in range(1, len(A)):
-        difference = abs(sum(A[:x]) - sum(A[x:]))
-        values.append(difference)
+# def solution(A):
+#     values = []
+#     for x in range(1, len(A)):
+#         difference = abs(sum(A[:x]) - sum(A[x:]))
+#         print(difference)
+#         values.append(difference)
     
-    return min(values)
+#     return min(values)
 
-A = [3, 1, 2, 4, 3]
+# def solution(A):
+#     chop_1 = A[0]
+#     chop_2 = sum(A[1:])
+#     min_diff = abs(chop_1 - chop_2)
+#     for i in range(1, len(A) - 1):
+#         chop_1 += A[i]
+#         chop_2 -= A[i]
+#         if abs(chop_1 - chop_2) < min_diff:
+#             min_diff = abs(chop_1 - chop_2)
+
+#     return min_diff
+
+def solution(A):
+    left, right = 0, sum(A)
+    best = float('Inf')
+    for a in A[:-1]:
+        left += a
+        right -= a
+        best = min(best, abs(left - right))
+
+    return best
+
+# A = [3, 1, 2, 4, 3]
+A = [-1000, 1000]
 print(solution(A))
